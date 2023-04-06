725b2fe6f3035a32a33bbd627266b371f60f3e5c
MadsenWalker <madsenwalker@gmail.com>
2023-04-06 14:59:56 -0600

    traceability 4.6.23.Lab

---
+++ _build/doctrees/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+
+app.use(express.json());
+app.use(cors());
+
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '8971b574fd1f47848943a5cf2d2ba8b0',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
